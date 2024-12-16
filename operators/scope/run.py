4b121d221c9ac385bbc63b4c97257328c77ce9ee
Sam Welborn <swelborn@lbl.gov>
2024-12-16 17:16:36 -0500

    init operator run.py

---
+++ operators/scope/run.py
@@ -0,0 +1,96 @@
+import multiprocessing
+import pathlib
+import time
+from typing import Any, cast
+
+import DTMicroscope.server.server_afm
+import numpy as np
+import Pyro5.api
+import Pyro5.errors
+from DTMicroscope.server.server_afm import main_server
+from pydantic import BaseModel
+
+from core.models.messages import BytesMessage, MessageHeader, MessageSubject
+from operators.operator import DATA_DIRECTORY, dependencies, operator
+
+data_dir = pathlib.Path(DATA_DIRECTORY) / "data"
+
+
+class DatasetInfo(BaseModel):
+    path: pathlib.Path
+    channels: list[str] = []
+    signals: list[str] = []
+    units: list[str] = []
+    scans: list[int] = []
+    spectra: list[Any] = []
+    point_clouds: list[Any] = []
+
+    @classmethod
+    def from_info(cls, info, path: pathlib.Path):
+        info_dict = {item[0]: item[1] for item in info}
+
+        try:
+            return cls(
+                path=path,
+                channels=info_dict["channels"],
+                signals=info_dict["signals"],
+                units=info_dict["units"],
+                scans=info_dict["scans"],
+                spectra=info_dict["spectra"],
+                point_clouds=info_dict["point_clouds"],
+            )
+        except KeyError as e:
+            raise ValueError(f"Missing key in info: {e}")
+
+
+URI = "PYRO:microscope.server@localhost:9092"
+MIC_SERVER = Pyro5.api.Proxy(URI)
+MIC_SERVER = cast(DTMicroscope.server.server_afm.MicroscopeServer, MIC_SERVER)
+data_dir = pathlib.Path(DATA_DIRECTORY) / "data"
+filenames = data_dir.glob("*.h5")
+H5_FILES = cast(list, cast(str, [str(data_dir / f) for f in filenames]))
+the_file = H5_FILES[0]
+the_dataset_info: DatasetInfo = DatasetInfo(path=pathlib.Path(the_file))
+the_data = np.random.rand(10, 10)
+
+
+@dependencies
+def deps():
+    process = multiprocessing.Process(target=main_server)
+    process.start()
+    global MIC_SERVER, the_data, the_dataset_info
+    retries = 0
+    max_retries = 5
+    while True:
+        try:
+            MIC_SERVER.initialize_microscope("AFM", data_path=str(the_file))
+            MIC_SERVER.setup_microscope()
+            the_dataset_info = DatasetInfo.from_info(
+                MIC_SERVER.get_dataset_info(), the_file
+            )
+
+            data = MIC_SERVER.get_scan(channels=["HeightRetrace"])
+            if data is None:
+                raise ValueError("No data returned from get_scan")
+            array_list, shape, dtype = data
+            the_data = np.array(array_list, dtype=dtype).reshape(shape)
+            print("INITIALIZED MICROSCOPE")
+            break
+        except Pyro5.errors.CommunicationError:
+            retries += 1
+            time.sleep(1)
+            if retries > max_retries:
+                raise
+    yield
+    process.terminate()
+    process.join()
+
+
+@operator
+def afm_microscope(
+    inputs: BytesMessage | None, parameters: dict[str, Any]
+) -> BytesMessage | None:
+    global MIC_SERVER, the_dataset_info, the_data
+    meta = the_dataset_info.model_dump()
+    header = MessageHeader(subject=MessageSubject.BYTES, meta=meta)
+    return BytesMessage(header=header, data=the_data.tobytes())
