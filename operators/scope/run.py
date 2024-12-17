53434c420665472b7f4eab2f512d480020ebe727
Sam Welborn <swelborn@lbl.gov>
2024-12-16 23:09:50 -0500

    include shape+dtype information and remove path from DatasetInfo

--- operators/scope/run.py
+++ operators/scope/run.py
@@ -7,17 +7,21 @@ import DTMicroscope.server.server_afm
 import numpy as np
 import Pyro5.api
 import Pyro5.errors
-from DTMicroscope.server.server_afm import main_server
-from pydantic import BaseModel
-
 from core.models.messages import BytesMessage, MessageHeader, MessageSubject
+from DTMicroscope.server.server_afm import main_server
 from operators.operator import DATA_DIRECTORY, dependencies, operator
+from pydantic import BaseModel
 
 data_dir = pathlib.Path(DATA_DIRECTORY) / "data"
 
 
-class DatasetInfo(BaseModel):
+class OtherMetadata(BaseModel):
     path: pathlib.Path
+    shape: tuple[int, ...]
+    dtype: str
+
+
+class DatasetInfo(BaseModel):
     channels: list[str] = []
     signals: list[str] = []
     units: list[str] = []
@@ -26,12 +30,11 @@ class DatasetInfo(BaseModel):
     point_clouds: list[Any] = []
 
     @classmethod
-    def from_info(cls, info, path: pathlib.Path):
+    def from_info(cls, info):
         info_dict = {item[0]: item[1] for item in info}
 
         try:
             return cls(
-                path=path,
                 channels=info_dict["channels"],
                 signals=info_dict["signals"],
                 units=info_dict["units"],
@@ -50,7 +53,7 @@ data_dir = pathlib.Path(DATA_DIRECTORY) / "data"
 filenames = data_dir.glob("*.h5")
 H5_FILES = cast(list, cast(str, [str(data_dir / f) for f in filenames]))
 the_file = H5_FILES[0]
-the_dataset_info: DatasetInfo = DatasetInfo(path=pathlib.Path(the_file))
+the_dataset_info: DatasetInfo = DatasetInfo()
 the_data = np.random.rand(10, 10)
 
 
@@ -65,15 +68,14 @@ def deps():
         try:
             MIC_SERVER.initialize_microscope("AFM", data_path=str(the_file))
             MIC_SERVER.setup_microscope()
-            the_dataset_info = DatasetInfo.from_info(
-                MIC_SERVER.get_dataset_info(), the_file
-            )
+            the_dataset_info = DatasetInfo.from_info(MIC_SERVER.get_dataset_info())
 
             data = MIC_SERVER.get_scan(channels=["HeightRetrace"])
             if data is None:
                 raise ValueError("No data returned from get_scan")
             array_list, shape, dtype = data
             the_data = np.array(array_list, dtype=dtype).reshape(shape)
+            the_data = the_data[0]
             print("INITIALIZED MICROSCOPE")
             break
         except Pyro5.errors.CommunicationError:
@@ -91,6 +93,10 @@ def afm_microscope(
     inputs: BytesMessage | None, parameters: dict[str, Any]
 ) -> BytesMessage | None:
     global MIC_SERVER, the_dataset_info, the_data
-    meta = the_dataset_info.model_dump()
+    meta1 = the_dataset_info.model_dump()
+    meta2 = OtherMetadata(
+        path=the_file, shape=the_data.shape, dtype=str(the_data.dtype)
+    ).model_dump()
+    meta = {**meta1, **meta2}
     header = MessageHeader(subject=MessageSubject.BYTES, meta=meta)
     return BytesMessage(header=header, data=the_data.tobytes())
