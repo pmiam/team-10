0c65863b3597d5ac8d590acaca3f195bd24a7351
Sam Welborn <swelborn@lbl.gov>
2024-12-17 14:10:09 -0500

    add normalize parameter
    - this normalizes the data inside the segment_cellular_image fct (it is an argument there).
    - we can control this from the frontend.

--- operators/segmentation/run.py
+++ operators/segmentation/run.py
@@ -19,7 +19,7 @@ accumulated_dense = None
 class OtherMetadata(BaseModel):
     model_config: ConfigDict = ConfigDict(extra="ignore")
     path: pathlib.Path
-    shape: tuple[int, int]
+    shape: tuple[int, ...]
     dtype: str
 
 
@@ -27,28 +27,40 @@ def normalize_image(data: np.ndarray) -> np.ndarray:
     return (data - np.min(data)) / np.ptp(data)
 
 
-def select_samples_grain_size():
-    pass
+def str_to_bool(s):
+    if isinstance(s, bool):
+        return s
+    if isinstance(s, str):
+        return s.lower() == "true"
+    raise ValueError("Input must be a string")
 
 
 @operator
 def segmenter(
     inputs: BytesMessage | None, parameters: dict[str, Any]
 ) -> BytesMessage | None:
-    if not inputs:
+    if inputs is None:
         return None
 
     other_metadata = OtherMetadata(**inputs.header.meta)
-
     data = np.frombuffer(inputs.data, dtype=other_metadata.dtype).reshape(
         other_metadata.shape
     )
 
     if data.ndim == 3:
+        print("Data is 3D, selecting first channel")
         data = data[0]
 
     assert data.ndim == 2, f"Data must be 2D, got {data.ndim}D data"
 
-    data = normalize_image(data)
+    meta = OtherMetadata(
+        path=other_metadata.path, shape=data.shape, dtype=str(data.dtype)
+    )
+    header = inputs.header
+    header.meta.update(meta.model_dump())
+    normalize = parameters.get("normalize", True)
+    normalize = str_to_bool(normalize)
 
-    mask, embedding, bounding_boxes = segment_cellular_image(data)
+    print(f"Segmenting image with parameters: {parameters}")
+    mask, _, _ = segment_cellular_image(data, normalize=normalize)
+    return BytesMessage(header=header, data=mask.tobytes())
