18c9842a6d0bd371c2fc0353a2f7a93c85d48ea4
Mikolaj Jakowski <mikojakowski@gmail.com>
2024-12-17 20:46:11 -0500

    code to find average orientation of the grains

---
+++ get_average_orientation.py
@@ -0,0 +1,24 @@
+#function to find the average orientation of the grains
+import numpy as np
+
+def get_avg_orientation(mask : np.ndarray) -> float:
+    slopes_list = []
+
+    #mask value 0 is just the background
+    num_grains = len(np.unique(mask)) -1
+
+    for i in range(1, num_grains+1):
+        grain_coords = []
+
+        indexes = np.where(mask == i)
+        indexes_x = indexes[0]
+        indexes_y = indexes[1]
+
+        m, b = np.polyfit(indexes_x, indexes_y, 1)
+
+        slopes_list.append(m)
+
+    slopes_array = np.array(slopes_list)
+
+    return np.average(slopes_array)
+
